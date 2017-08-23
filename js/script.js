ee01918d46f75b9c601612c5212f68fb73a0c576
michaelmnolla <michael_mn153@hotmail.com>
2017-08-23 04:59:51 +0200

    nuevo archivo con JAVA

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var contenedor = document.querySelector("#contenedor");
 var lienzo = document.querySelector("#lienzo");
+var btnAmpliar = document.querySelector("#btnAmpliar");
 
 
 function ampliar(){
@@ -13,4 +14,32 @@ function ampliar(){
 	lienzo.style.backgroundSize = "cover";
 	lienzo.style.backgroundRepeat = "no-repeat";
 
+
+	btnAmpliar.innerHTML = "Reducir";
+	btnAmpliar.style.position = "fixed";
+	btnAmpliar.style.top ="10px";
+	btnAmpliar.style.left = "10px";
+	btnAmpliar.style.zIndex = "1";
+
+	btnAmpliar.setAttribute("onClick" , "Reducir()");
 }
+
+function Reducir(){
+
+	contenedor.style.width = "1000px";
+	contenedor.style.height = "500px";
+	contenedor.style.margin = "5vh auto";
+
+	lienzo.style.width = "1000px";
+	lienzo.style.height = "500px";
+
+	btnAmpliar.innerHTML = "ampliar";
+	btnAmpliar.style.position = "relative";
+	btnAmpliar.style.top ="0";
+	btnAmpliar.style.left = "0";
+	btnAmpliar.style.zIndex = "0";
+
+	btnAmpliar.setAttribute("onClick" , "ampliar()");
+	
+
+	}
